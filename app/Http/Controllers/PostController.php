285ce92aa25e0414160daa70379f42790c9fbfa3
hossam255 <hossam255@gmail.com>
2022-11-29 22:51:52 +0200

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
    
     public function index()
     {
-        return view('blog.index');
+        
+        return view('blog.index')
+        ->with('posts', Post::get());
     }
 
    
--- database/migrations/2022_11_29_093004_posts.php
+++ database/migrations/2022_11_29_093004_posts.php
@@ -6,21 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 class Posts extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+   
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->reference('id')->on('users');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+   
     public function down()
     {
         //
